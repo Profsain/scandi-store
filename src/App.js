2078d75fd43870aea35ab81f13e7c253b48fcbff
Profsain <profsaincrown@gmail.com>
2022-10-24 12:16:35 +0100

    Storing data to redux store

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { GraphQLClient } from 'graphql-request';
+import {LOAD_DATA} from './graphQL/Queries';
+import { getProductsStart, getProductsSuccess, getProductsFailure } from './redux/features/ProductsSlice';
 import './App.css';
 
-import React, { Component } from 'react'
 
-export default class App extends Component {
+class App extends Component {
+
+  // Load data and store in redux on component mount
+  componentDidMount() {
+    
+    const client = new GraphQLClient('http://localhost:4000/graphql', {
+      headers: {
+        authorization: 'Bearer my-token',
+      },
+    }) 
+    client.request(LOAD_DATA).then(data => {
+      console.log('data', data.categories[0].products);
+      getProductsSuccess(data.categories.products);
+    });
+  }
+
   render() {
+    const { products, loading, error } = this.props;
+    console.log('products', products);
     return (
       <div>
         <h1>GraphQL Ecommerce Store</h1>
       </div>
-    )
+    );
   }
 }
 
+const mapStateToProps = (state) => {
+  return {
+    products: state.products.products,
+    categories: state.products.categories,
+    cart: state.products.cart,
+    loading: state.products.loading,
+    error: state.products.error,
+  }
+}
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -3,50 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { 
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  HttpLink,
-  from} from '@apollo/client'
-  import { onError } from '@apollo/client/link/error';
-  import { store } from './redux/App';
-  import { Provider } from 'react-redux';
-  import { request, GraphQLClient } from 'graphql-request';
-  import { LOAD_DATA } from './graphQL/Queries';
+import { store } from './redux/App';
+import { Provider } from 'react-redux';
 
-
-  // Error Handling
-  // const errorLink = onError(({ graphQLErrors, networkError }) => {
-  //   if (graphQLErrors) {
-  //     graphQLErrors.map(({ message, locations, path }) =>
-  //       console.log(
-  //         `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
-  //       ),
-  //     );
-  //   }
-  // });
-
-  // // Server localhost link system
-  // const link = from([
-  //   errorLink,
-  //   new HttpLink({ uri: 'http://localhost:4000/graphql' }),
-  // ]);
-
-  // // client system
-  // const client = new ApolloClient({
-  //   cache: new InMemoryCache(),
-  //   link
-  // })
-  
-  // data request
-  const client = new GraphQLClient('http://localhost:4000/graphql', {
-    headers: {
-      authorization: 'Bearer my-token',
-    },
-  })
-
-  client.request(LOAD_DATA).then(data => console.log(data));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
