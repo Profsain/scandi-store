54e35af8b8d5f27bc3e13e918d7e03b41d96fd31
Profsain <profsaincrown@gmail.com>
2022-11-16 21:47:53 +0000

    Extracted Tech Category component

--- src/components/tech/TechCategory.js
+++ src/components/tech/TechCategory.js
@@ -1,63 +1,10 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { toggleShowProductDetails, setProductId } from '../../redux/ProductsSlice';
-import ProductCard from '../products/ProductCard';
-import ProductDetails from '../productDetails/ProductDetails';
-import { currencyChangesHandler } from '../helper';
-import '.././products/Products.css';
+import ProductsList from '../products/ProductsList';
 
-class TechCategory extends Component {
+export default class TechCategory extends Component {
   render() {
-    const { productsStore, toggleShowProductDetails, setProductId } = this.props;
-    const { products, showProductDetails, productId } = productsStore.productsReducer;
-    const label = productsStore.cartReducer.currency;
-
-    // open product details page
-    const openProductDetails = (id) => {
-      toggleShowProductDetails();
-      setProductId(id);
-    }
-
     return (
-      <>
-        <div>
-          <h2>Tech Store</h2>
-          <div className='Products-grid'>
-            {
-              products && products.categories[2].products.map(({ id, name, gallery, prices, inStock }) => (
-                <div key={id} onClick={() => openProductDetails(id)}>
-                  <ProductCard
-                    key={id}
-                    name={name}
-                    img={gallery[0]}
-                    inStock={inStock}
-                    productCost={currencyChangesHandler(prices, label)}
-                  />
-                </div>
-              ))
-            }
-          </div>
-        </div>
-        
-        {showProductDetails
-          && <ProductDetails
-            togglePage={toggleShowProductDetails}
-            productId={productId}
-          />
-        }
-      </>
+      <ProductsList catIndex={2}/>
     )
   }
 }
-
-
-const mapStateToProps = (state) => ({
-  productsStore: state,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  toggleShowProductDetails: () => dispatch(toggleShowProductDetails()),
-  setProductId: (id) => dispatch(setProductId(id)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(TechCategory);
